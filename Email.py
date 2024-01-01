b549c443c6dc9a8e81f79378a81081705dabae0d
bryanvengwa <bmvengwa@gmail.com>
2024-01-02 01:13:31 +0200

    initial commit

---
+++ Email.py
@@ -0,0 +1,31 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Email configuration
+sender_email = "bmvengwa@gmail.com"
+receiver_email = "bmvengwa@gmail.com"
+# receiver_email = "morganreese671@gmail.com"
+subject = "Subject of the email"
+body = "Body of the email"
+
+# Create the MIME object
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+
+# Attach the body to the email
+message.attach(MIMEText(body, "plain"))
+
+# Connect to the SMTP server (Gmail example)
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+username = "bmvengwa@gmail.com"
+password = "rowh mycx lhnb ocvj4"
+
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+print("done")
