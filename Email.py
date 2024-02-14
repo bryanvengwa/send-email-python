da82469d693b166597fb52b2cbdabb8de309ab8e
bryanvengwa <bmvengwa@gmail.com>
2024-02-14 09:48:53 +0200

    initial commit

--- Email.py
+++ Email.py
@@ -1,3 +1,4 @@
+
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -5,7 +6,6 @@ from email.mime.multipart import MIMEMultipart
 # Email configuration
 sender_email = "bmvengwa@gmail.com"
 receiver_email = "bmvengwa@gmail.com"
-# receiver_email = "morganreese671@gmail.com"
 subject = "Subject of the email"
 body = "Body of the email"
 
@@ -21,11 +21,12 @@ message.attach(MIMEText(body, "plain"))
 # Connect to the SMTP server (Gmail example)
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
-username = "bmvengwa@gmail.com"
-password = "rowh mycx lhnb ocvj4"
+username = "your email  username"
+password = "your app name"
 
 with smtplib.SMTP(smtp_server, smtp_port) as server:
     server.starttls()
     server.login(username, password)
     server.sendmail(sender_email, receiver_email, message.as_string())
 print("done")
+
